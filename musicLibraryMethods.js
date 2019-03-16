63c4b92bbbb8a4245e018a4216974b133b385ff1
GideonBrasil <gidxx_z@yahoo.com>
2019-03-16 02:18:45 +0000

    correct playlists array and push playlists function

--- musicLibraryMethods.js
+++ musicLibraryMethods.js
@@ -1,19 +1,22 @@
 let Library = function (name, creator) {
     this.name = name;
     this.creator = creator;
-    this.Playlist = [];
+    this.playlists = [];
 }
 
 Library.prototype.addPlaylist = function (listObject) {
-    this.playlists.push.(listObject);
+    this.playlists.push(listObject);
 };
 
 let Playlist = function (name) {
     this.name = name;
-
+    this.tracks = [];
 }
 
-let Track = function (title, rating) {
+Playlist.prototype.overallRating
+
+let Track = function (title, rating, length) {
     this.title = title;
     this.rating = rating;
+    this.length = length;
 }
