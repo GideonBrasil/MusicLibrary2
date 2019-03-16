ed7eef02914cff89542c954e56554074a4a02367
GideonBrasil <gidxx_z@yahoo.com>
2019-03-16 02:14:06 +0000

    Playlist objects can be added to an instance of a library

--- musicLibraryMethods.js
+++ musicLibraryMethods.js
@@ -1,4 +1,19 @@
 let Library = function (name, creator) {
     this.name = name;
     this.creator = creator;
+    this.Playlist = [];
+}
+
+Library.prototype.addPlaylist = function (listObject) {
+    this.playlists.push.(listObject);
+};
+
+let Playlist = function (name) {
+    this.name = name;
+
+}
+
+let Track = function (title, rating) {
+    this.title = title;
+    this.rating = rating;
 }
